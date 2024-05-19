5ec1e51fa4191aa9336ea18257eba9fca37a24cf
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2024-05-19 00:53:44 -0300

    add question resto2

---
+++ resto2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+    cin>>n;
+
+    for(int i=0; i<10000; i++){
+        if(i%n == 2){
+            cout<<i<<endl;
+        }
+    }
+    system("pause");
+    return 0;
+}
